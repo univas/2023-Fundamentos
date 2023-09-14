f613c0a694b866ceb26b62c4e1aea154ee9ab2ce
Marcos Antônio dos Santos <marcaosi2014@gmail.com>
2023-09-13 22:13:16 -0300

    Add files via upload

---
+++ ExerciciosLacosRepeticao.c
@@ -0,0 +1,30 @@
+/**
+ * 
+ * 1.	Faça	um	programa	em	C	que	mostre	a	 tabuada	do número	5.	 (considerar	tabuada	do		número	1	ao	10).
+ * 
+ * 	
+ * 2.	 Faça	 um	 programa	 em	 C	 que	 mostre	 a	 tabuada	 de	 qualquer	 número	escolhido	pelo	usuário	(considerar	tabuada	do	número	1	ao	10).	
+ * 
+ * 
+ * 3. 	 Faça	 um	 programa	 em	 C	 (utlize	 a	 estrutura	 for)	 que	 leia	 10	 valores	inteiros	e:	
+•	Encontre	e	mostre	o	maior	valor		
+•	Encontre	e	mostre	o	menor	valor		
+•	Calcule	e	mostre	a	média	dos	números	lidos
+ *
+ * 4. Faça um programa em C que dado um número inteiro digitado pelo usuário mostre todos os números primos entre 1 e o número digitado.
+ * 
+ * 5. Faça um programa em C que solicite a idade de 20 pessoas e:
+ * - Calcule a idade média entre elas
+ * - Calcule a menor idade
+ * - Calcule a maior idade
+ * - Calcule quantas pessoas são maiores de idade
+ * 
+ * 
+ * 6. Uma		loja		ualiza		o		código		V		para		compras		à		vista		e		o	código		P		para		compras		a		prazo.		Faça		um	algoritmo		que	receba	o	código	de		e	o	valor	de	15	transações.	Calcule	e	mostre:	
+• O	valor	total	das	compras	à	vista	
+• O	valor	total	das	compras	a	prazo	
+• O	valor	total	das	compras	efetuadas	
+ *
+ *
+ * 
+*/
